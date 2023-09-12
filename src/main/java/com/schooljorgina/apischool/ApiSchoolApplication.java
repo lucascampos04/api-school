b5bc382f7d3c7ab8975ddf640b58f2d2fd7e6faa
lucascampos04 <camposdlucasoli@gmail.com>
2023-09-12 19:40:05 -0300

    First commit

---
+++ src/main/java/com/schooljorgina/apischool/ApiSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.schooljorgina.apischool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSchoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiSchoolApplication.class, args);
+    }
+
+}

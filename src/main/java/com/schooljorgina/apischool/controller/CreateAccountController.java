768321afea94e6a726936a12a2850ee22dca9022
lucascampos04 <camposdlucasoli@gmail.com>
2023-09-12 21:11:00 -0300

    logic of routers

--- src/main/java/com/schooljorgina/apischool/controller/CreateAccountController.java
+++ src/main/java/com/schooljorgina/apischool/controller/CreateAccountController.java
@@ -1,15 +1,17 @@
 package com.schooljorgina.apischool.controller;
 
 import com.schooljorgina.apischool.model.CreateAccount;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("/criar-conta")
+@RequestMapping("/")
 public class CreateAccountController {
 
     @GetMapping
@@ -18,10 +20,20 @@ public class CreateAccountController {
         return "createAccount";
     }
 
-    @PostMapping("/conta-criado-com-sucesso")
-    public ModelAndView saveUser(CreateAccount createAccount){
-        ModelAndView mv = new ModelAndView("msgSuccess");
-        mv.addObject("message", "Conta criado com sucesso");
+    @PostMapping("/conta-criada")
+    public Object saveProject(@Valid CreateAccount createAccount, BindingResult bindingResult) {
+        ModelAndView mv = new ModelAndView("success");
+
+        mv.addObject("message", "Conta criada com sucesso");
+        if (bindingResult.hasErrors()){
+            return "redirect:/";
+        }
+
+        if (createAccount.getNome().isEmpty()){
+            System.out.println("O nome é obrigatorio");
+            mv.addObject("error", "Nome é obrigatorio");
+            return mv;
+        }
         return mv;
     }
 
